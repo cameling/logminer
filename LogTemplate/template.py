7888937bd4036f4b2d1cbf02212950622b29c23c
Lin <Lin@Home>
2015-12-05 23:43:30 +0800

    Polished code with new README

--- LogTemplate/template.py
+++ LogTemplate/template.py
@@ -1,35 +1,50 @@
 import argparse
 import json
 import re
-import string
 import sys
 from collections import defaultdict
-from LogTemplate.stopwords import words, punct
+from LogTemplate.stopwords import stopwords, punct
 
 def parse_args():
     argument_parse = argparse.ArgumentParser(description = "Clustering based log template miner.")
-    argument_parse.add_argument("-s", action = "store", dest = "support", type = int, help = "A positive number of support")
-    argument_parse.add_argument("-p", action = "store", dest = "path", help = "Path to the log file")
+    argument_parse.add_argument("-s", action = "store", dest = "support", type = int, default = 50, help = "A positive number of support")
+    argument_parse.add_argument("-f", action = "store", dest = "logfile", default = "./sample/access_log", help = "The log file to analyze")
+    argument_parse.add_argument("-p", action = "store", dest = "peeler", default = "./peeler/access_log", help = "Log file's message peeler")
     results = argument_parse.parse_args()
     support = results.support
-    filepath = results.path
-    return support, filepath
+    logfilepath = results.logfile
+    peelerpath = results.peeler
+    return support, logfilepath, peelerpath
 
-def build_vocabulary(filepath):
+def get_peeler(peelerpath):
+    with open(peelerpath, 'r') as pfin:
+        line = pfin.readline()
+        try:
+            peeler = json.loads(line)
+        except ValueError:
+            print "--->Input file format error!"
+            sys.exit(1)
+    return peeler       
+
+def build_vocabulary(logfilepath, peeler):
     vocabulary = defaultdict(int)
-    with open(filepath, 'r') as fin:
+    outer_delimiter = peeler["outer_delimiter"]
+    inner_delimiter = peeler["inner_delimiter"]
+    start = int(peeler["message_start"])
+    end = peeler["message_end"]
+    if end != "":
+        end = int(end)
+    else:
+        end = None
+    with open(logfilepath, 'r') as fin:
         for line in fin:
-            try:
-                json_line = json.loads(line)
-            except ValueError:
-                print "--->Input file format error!"
-                sys.exit(1)
-            message = json_line["message"].strip()
-            words = message.split()
-            for i, word in enumerate(words):
-                vocabulary[(word, i)] += 1
-    eval_vocab = {k:v for (k,v) in vocabulary.iteritems() if evaluate_vocabulary(k)}
-    return eval_vocab
+            line = line.strip()
+            message = outer_delimiter.join(line.split(outer_delimiter)[start:end])
+            tokens = re.split(inner_delimiter, message)
+            tokens = filter(lambda a: a != "", tokens)
+            for i, token in enumerate(tokens):
+                vocabulary[(token, i)] += 1
+    return vocabulary
 
 # Template length > 0 after all stop words and punctuation are filtered.
 def evaluate_template(template):
@@ -38,21 +53,13 @@ def evaluate_template(template):
         return False
     for curr_tuple in template:
         curr_word = curr_tuple[0]
-    if curr_word in words or curr_word in punct:
+    if curr_word in stopwords or curr_word in punct:
         if curr_word in punct:
             template_len -= 1
     if template_len > 0:
         return True
     else:
         return False
-
-# Vocabulary contains not only numbers and punctuation
-def evaluate_vocabulary(vocabulary):
-    vocab_text = vocabulary[0]
-    for i in vocab_text:
-        if not (i.isdigit() or i in string.punctuation):
-            return True
-    return False
 
 def transform_regex(template):
     place_holder = "YLPLACEHOLDER"
@@ -77,36 +84,51 @@ def transform_regex(template):
         else:
             temp_list = [token[0] for token in curr_list]
             temp_str = " ".join(temp_list)
-        regex_str = re.escape(temp_str)
-        regex_str = regex_str.replace(place_holder, "(.*)")
+        regex_str = temp_str.replace(place_holder, "*")
         temp_regex.append(regex_str)
     sorted_temp_regex = sorted(temp_regex, key = lambda x : len(x), reverse = True)
     return sorted_temp_regex     
         
         
-def build_template(filepath, vocabulary, support):
+def build_template(logfilepath, vocabulary, support, peeler):
     voc_by_sup = {k:v for (k,v) in vocabulary.iteritems() if v >= support}
-    with open(filepath, 'r') as fin:
+    outer_delimiter = peeler["outer_delimiter"]
+    inner_delimiter = peeler["inner_delimiter"]
+    start = int(peeler["message_start"])
+    end = peeler["message_end"]
+    if end != "":
+        end = int(end)
+    else:
+        end = None
+    with open(logfilepath, 'r') as fin:
         template = defaultdict(int)
         for line in fin:
-            try:
-                json_line = json.loads(line)
-            except ValueError:
-                print "--->Input file format error!"
-                sys.exit(1)
-            message = json_line["message"].strip()
-            words = message.split()
+            line = line.strip()
+            message = outer_delimiter.join(line.split(outer_delimiter)[start:end])
+            tokens = re.split(inner_delimiter, message)
+            tokens = filter(lambda a: a != "", tokens)
             curr_list = []
-            for i, word in enumerate(words):
-                if (word, i) in voc_by_sup:
-                    curr_list.append((word, i))
+            for i, token in enumerate(tokens):
+                if (token, i) in voc_by_sup:
+                    curr_list.append((token, i))
             if evaluate_template(curr_list):
                 curr_tuple = tuple(curr_list)
                 template[curr_tuple] += 1
     tem_by_sup = {k:v for (k,v) in template.iteritems() if v >= support}
-    return tem_by_sup  
+    
+    # Filter out short templates who are contained in others
+    def not_contained(tem, all_tem):
+        to_compare = set(tem)
+        for curr_tem in all_tem:
+            curr_set = set(curr_tem)           
+            if to_compare.issubset(curr_set) and not curr_set.issubset(to_compare):
+                return False
+        return True
+    tem_filtered = filter(lambda a: not_contained(a, tem_by_sup), tem_by_sup)
+    
+    return tem_filtered  
 
-def output_template(filepath, regex, support):
+def output_template(outputpath, regex, support):
     output_list = []
     for i, v in enumerate(regex):
         temp_item = {}
@@ -115,22 +137,26 @@ def output_template(filepath, regex, support):
         temp_item["template"] = v 
         output_list.append(temp_item)
     output_json = json.dumps(output_list, sort_keys=True, indent=4, separators=(',', ': '))
-    with open(filepath, 'w') as fout:
+    with open(outputpath, 'w') as fout:
         fout.write(output_json)
 
 if __name__ == "__main__":
-    support, filepath = parse_args()
+    support, logfilepath, peelerpath = parse_args()
+    
+    print "Retrieve message peeler."
+    peeler = get_peeler(peelerpath)
     
     print "Build vocabulary."
-    vocabulary = build_vocabulary(filepath)
-    
-    print "Summarize Template."
-    template = build_template(filepath, vocabulary, support)
-    
+    vocabulary = build_vocabulary(logfilepath, peeler)
+     
+    print "Summarize template."
+    template = build_template(logfilepath, vocabulary, support, peeler)
+     
     print "Convert to Regex."
     regex = transform_regex(template)
-     
-    output_filepath = filepath + "_template_" + str(support)
+      
+    output_filepath = logfilepath + "_template_" + str(support)
     print "Write templates to file " + output_filepath + "."
     output_template(output_filepath, regex, support)
+    
     print "Done."
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# logminer
-Mine patterns from logs
+logminer
+=======
+A set of tools for log analytics, from data processing to advanced modeling. Tools are organized in folders with sample data to play with. Brief instructions are documented here.
+
+###LogTemplate
+The first step to do any analytics against semi-structured logs is getting **templates** to handle them. Templates could be in many forms. The most popular form is a regular expression. With templates, free-text log messages become structured and are easy to consumed by analytics algorithms.
+
+This tool using clustering techniques to mine templates from history logs. Here's the usage:
+```
+usage: template.py [-h] [-s SUPPORT] [-f LOGFILE] [-p PEELER]
+
+Clustering based log template miner.
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -s SUPPORT  A positive number of support
+  -f LOGFILE  The log file to analyze
+  -p PEELER   Log file's message peeler
+```
+* Support is an integer value that tunes mined templates' quality. Higher value means more samples support the template, but you get less templates, vice versa. 
+* Peeler is a simple configuration tells the tool where to to look at in a line of raw log, and how to analyze. 
+
+For example, the sampled peeler is for a typical access log, to analyze visited URLs. It's a json in *one line* with four key-value pairs.
+```
+{"outer_delimiter":" ","inner_delimiter":" |/|\"","message_start":"5","message_end":"-2"}
+```
+* *outer_delimiter*, *message_start* and *message_end* locate the message you'd like to analyze. Delimiters are designated following the regular expression fashion. Usually a whitespace (" ") will do the trick. Start and end positions follow the Python list fashion. If the message's end position is EOF (End of Line), just use a blank string ("").
+* *inner_delimiter* tells the tool how to analyze the message. In this case, as URL is delimited by "/" so we add it in.
+
+The exampled input log file and output templates with support set to 50 are in the sample folder.
